93f11fd08b4e9c78b6e314bf54b28d34d29d05c4
ZachPerry197 <zperry@ucsd.edu>
2022-04-29 11:28:19 -0400

    test1

--- test1.java
+++ test1.java
@@ -1,8 +1,8 @@
 public class test1 {
 
-
-
-
+    public static int multiply(int int1, int int2) {
+        return int1 * int2;
+    }
 
 
 }
---
+++ test1test.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class test1test {
+    
+    @Test
+    public void multiply() {
+        assertEquals(4, test1.multiply(2, 3));
+    }
+
+
+
+}
